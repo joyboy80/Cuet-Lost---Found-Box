8de68d240c65b2783bb7b7096308695d2be3779a
joyboy80 <146458548+joyboy80@users.noreply.github.com>
2026-04-26 09:29:28 +0600

    Fix formatting issue in index.js

--- Mail Sender/index.js
+++ Mail Sender/index.js
@@ -10,7 +10,7 @@ const allowedOrigins = new Set([
   'http://127.0.0.1:5000',
   'http://localhost:5000',
   'https://cuet-lost-found-box.onrender.com',
-  'https://cuet-lost-found-box-1.onrender.com'
+  'https://cuet-lost-found-box-1.onrender.com',
   'https://cuet-lost-found-box-production.up.railway.app'
 ]);
 
